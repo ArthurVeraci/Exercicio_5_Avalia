3d6cb09c07e6d7805a1c40eec1e6520de86cda48
ArthurVeraci <arthurveraci5@gmail.com>
2022-07-06 16:37:51 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio_5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numeros de 1 a 50: ");
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(i * 2);
+            }
+            Console.ReadKey();
+        }
+    }
+}
